4d7493d808b34a65ef582bd95b13cecdec25a9f7
imgs <22imgs21212222@gmail.com>
2023-12-04 11:06:54 -0500

    Fixed Formatting + added back the readme

--- src/main/java/org/group3/Main.java
+++ src/main/java/org/group3/Main.java
@@ -3,14 +3,15 @@ package org.group3;
 import com.google.gson.Gson;
 import org.group3.model.DataModel;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+// Press Shift twice to open the Search Everywhere dialog and type `show
+// whitespaces`, then press Enter. You can now see whitespace characters in your
+// code.
 public class Main {
-    public static void main(String[] args) {
-        try {
-            DataModel.generateProgramList();
-        } catch (Exception e) {
-            System.out.println("Something went wrong...");
-        }
+  public static void main(String[] args) {
+    try {
+      DataModel.generateProgramList();
+    } catch (Exception e) {
+      System.out.println("Something went wrong...");
     }
+  }
 }
--- src/main/java/org/group3/model/DataModel.java
+++ src/main/java/org/group3/model/DataModel.java
@@ -2,29 +2,31 @@ package org.group3.model;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-
 import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.List;
 
 public class DataModel {
-    public static final String universityProgramsPath = "assets/data/programs.json";
-    public static List<UniversityProgram> generateProgramList() throws IOException {
-        FileReader reader = new FileReader(universityProgramsPath);
+  public static final String universityProgramsPath =
+      "assets/data/programs.json";
+  public static List<UniversityProgram> generateProgramList()
+      throws IOException {
+    FileReader reader = new FileReader(universityProgramsPath);
 
-        Gson gson = new Gson();
-        Type universityProgramListType = new TypeToken<List<UniversityProgram>>() {}.getType();
+    Gson gson = new Gson();
+    Type universityProgramListType =
+        new TypeToken<List<UniversityProgram>>() {}.getType();
 
-        List<UniversityProgram> universityProgramList = gson.fromJson(reader, universityProgramListType);
+    List<UniversityProgram> universityProgramList =
+        gson.fromJson(reader, universityProgramListType);
 
-        for (UniversityProgram program : universityProgramList) {
-            System.out.println(program);
-        }
-
-        System.out.println(universityProgramList.size());
-
-        return universityProgramList;
-
+    for (UniversityProgram program : universityProgramList) {
+      System.out.println(program);
     }
+
+    System.out.println(universityProgramList.size());
+
+    return universityProgramList;
+  }
 }
